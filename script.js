ece3b2b96bc64cb6c334dbdc0b5ff81f8fab7710
standwall5 <johnpatricksalen890@gmail.com>
2024-12-22 17:25:50 +0800

    Add disclaimer section, update navigation links, and implement scroll blur effect

---
+++ script.js
@@ -0,0 +1,33 @@
+// Blur nav on scroll
+$(window).scroll(function(e) {
+        
+    var distanceScrolled = $(window).scrollTop();
+
+    // $('nav').css('backdrop-filter', 'blur(' + distanceScrolled/100 + 'px)');  // property, value
+    
+
+    if(distanceScrolled > 100) {
+        $('nav').css({
+            backdropFilter: "blur(15px)",
+            WebkitBackdropFilter: "blur(15px)",
+            borderBottom: "1px solid rgba(55, 55, 55, 0.5)",
+            background: "rgba(0, 0, 0, 0.1)" // This can be used to overlay a color, still dont know if kevin powell's method is better tho
+        });
+    }
+    else {
+        $('nav').css({
+            backdropFilter: "none",
+            WebkitBackdropFilter: "none",
+            borderBottom: "none",
+            background: "none"
+        });
+    }
+    
+});
+
+
+// Close disclaimer
+function closeDisclaimer() {
+    document.getElementById('disclaimer').style.display = 'none';
+    document.querySelector('nav').style.top = '0';
+}
